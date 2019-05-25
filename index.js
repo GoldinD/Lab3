6d0f66f7d1a596c78ada4396f55140995079307f
Dean Goldin <1235950@students.wits.ac.za>
2019-05-25 23:18:38 +0200

    End of exercise 2.2 for lab 3

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-let path = require('path');
 let express = require('express');
 let app = express();
-let mainRouter = require('./routes/mainRoutes')
+let bodyParser = require('body-parser');
 
-app.use(mainRouter);
-app.listen(3000);
-console.log("Express server running on port 3000");
+// Loading our routers
+let mainRouter = require('./routes/mainRoutes')
+let classRouter = require('./routes/classRoutes')
+
+
+//tell express to use bodyParser for JSON and URL encoded form bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mounting our routes
+app.use("/", mainRouter);
+app.use("/class", classRouter);
+
+app.listen(3000)
+console.log("Express server running on port 3000")
